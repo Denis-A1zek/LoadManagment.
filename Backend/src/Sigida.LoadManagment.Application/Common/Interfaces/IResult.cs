ac2ce4a6f93fef816f024848f4037768e69e0b7e
Denis-A1zek <denissigida.work@gmail.com>
2023-05-25 19:36:45 +0300

    add result object from handlers

---
+++ Backend/src/Sigida.LoadManagment.Application/Common/Interfaces/IResult.cs
@@ -0,0 +1,15 @@
+﻿using Sigida.LoadManagment.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Application;
+
+public interface IResult<T>
+{
+    public T Payload { get; }
+    public string Reason { get; }
+    public bool IsSuccess { get; }
+}
---
+++ Backend/src/Sigida.LoadManagment.Application/Common/Models/Result.cs
@@ -0,0 +1,28 @@
+﻿using Sigida.LoadManagment.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Application.Common.Models;
+
+internal readonly record struct Result<T>(T Payload, string Reason) : IResult<T>
+{
+    private Result(string reason) : this(default, reason) 
+        => Reason = reason;
+
+    private Result(T payload) : this(payload, default) 
+        => Payload = payload;
+
+    public bool IsSuccess => Reason is not null;
+
+    internal static Result<T> Fail(string reason)
+        => new Result<T>(reason);
+
+    internal static Result<T> Success(T payload)
+        => new Result<T>(payload);
+
+    public static implicit operator bool(Result<T> result)
+        => result.IsSuccess;
+}
