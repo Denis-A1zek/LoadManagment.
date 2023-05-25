90589079ee8a4987859688470a7a4dfdedf9418e
Denis-A1zek <denissigida.work@gmail.com>
2023-05-25 19:37:01 +0300

    create plan_create_handler

---
+++ Backend/src/Sigida.LoadManagment.Application/Features/PlanFeature/CreatePlan/CreatePlanCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Sigida.LoadManagment.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Application.Features.PlanFeature.CreatePlan;
+
+public sealed record CreatePlanCommand : IRequest<IResult<Guid>>
+{
+    public CreatePlanCommand(string description,
+        DateTime? startDate = null,
+        DateTime? endDate = null)
+    {
+        Description = description;
+        StartDate = startDate ?? new DateTime(DateTime.Now.Year, 09, 1);
+        EndDate = endDate ?? StartDate.Value.AddMonths(10);
+    }
+
+    public string Description { get; init; } = null!;
+    public DateTime? StartDate { get; init; }
+    public DateTime? EndDate { get; init; }
+}
---
+++ Backend/src/Sigida.LoadManagment.Application/Features/PlanFeature/CreatePlan/CreatePlanCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Sigida.LoadManagment.Application.Common.Models;
+using Sigida.LoadManagment.Domain.Entities;
+using Sigida.LoadManagment.Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Application.Features.PlanFeature.CreatePlan;
+
+public class CreatePlanCommandHandler : IRequestHandler<CreatePlanCommand, IResult<Guid>>
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CreatePlanCommandHandler
+        (ApplicationDbContext context, IMapper mapper)
+        => (_context, _mapper) = (context, mapper);
+
+    public async Task<IResult<Guid>> Handle(CreatePlanCommand request, 
+        CancellationToken cancellationToken)
+    {
+        var plan = _mapper.Map<Plan>(request);
+
+        await _context.Plans.AddAsync(plan, cancellationToken);
+        await _context.SaveChangesAsync();
+
+        return Result<Guid>.Success(plan.Id);
+    }
+}
