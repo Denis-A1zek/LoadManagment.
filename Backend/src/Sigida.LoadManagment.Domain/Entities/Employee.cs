c2206dcf881201acbfa66a1099b54a334a25b560
Denis-A1zek <denissigida.work@gmail.com>
2023-05-26 13:53:26 +0300

    created_an_employee_entity

---
+++ Backend/src/Sigida.LoadManagment.Domain/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Domain.Entities;
+
+public class Employee : Identity
+{
+    public string Name { get; set; } = null!;
+    public string Surname { get; set; } = null!;
+    public string Lastname { get; set; } = null!;
+}
