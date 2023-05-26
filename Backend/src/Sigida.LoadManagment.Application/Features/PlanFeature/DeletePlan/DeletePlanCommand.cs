38c24d53f4463ae040724b2e7f9b3f0f0e7c5fd4
Denis-A1zek <denissigida.work@gmail.com>
2023-05-26 08:43:09 +0300

    add_delete_plan_handler

---
+++ Backend/src/Sigida.LoadManagment.Application/Features/PlanFeature/DeletePlan/DeletePlanCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Sigida.LoadManagment.Application.Common.Models;
+
+namespace Sigida.LoadManagment.Application.Features.PlanFeature.DeletePlan;
+
+public sealed record DeletePlanCommand(Guid Id) : IRequest<IResult<Guid>>;
+
---
+++ Backend/src/Sigida.LoadManagment.Application/Features/PlanFeature/DeletePlan/DeletePlanCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Sigida.LoadManagment.Application.Common.Exceptions;
+using Sigida.LoadManagment.Application.Common.Models;
+using Sigida.LoadManagment.Infrastructure.Database;
+
+namespace Sigida.LoadManagment.Application.Features.PlanFeature.DeletePlan;
+
+public sealed class DeletePlanCommandHandler : IRequestHandler<DeletePlanCommand, IResult<Guid>>
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public DeletePlanCommandHandler
+        (ApplicationDbContext context, IMapper mapper)
+        => (_context, _mapper) = (context, mapper);
+
+    public async Task<IResult<Guid>> Handle(DeletePlanCommand request, CancellationToken cancellationToken)
+    {
+        var plan = await _context.Plans.FirstOrDefaultAsync(
+            p => p.Id == request.Id);
+
+        if (plan is null)
+            return Result<Guid>.Fail($"{request.Id} not found");
+
+        _context.Plans.Remove(plan);
+        await _context.SaveChangesAsync();
+
+        return Result<Guid>.Success(plan.Id);
+    }
+}
