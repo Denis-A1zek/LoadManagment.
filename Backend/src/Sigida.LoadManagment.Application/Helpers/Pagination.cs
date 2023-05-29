4ef7b62299ed2f4ea6fb7bf1d082a38b47456d31
Denis-A1zek <denissigida.work@gmail.com>
2023-05-29 13:11:29 +0300

    aadd pagination halper

---
+++ Backend/src/Sigida.LoadManagment.Application/Helpers/Pagination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Application.Helpers
+{
+    internal static class Pagination
+    {
+        public static int CalculateNumberOfPages(int numberOfValues, int pageSize)
+            => (int)Math.Ceiling((decimal)numberOfValues / pageSize);
+
+        public static int CalculateNumberOfSkips(int numberOfPage, int pageSize)
+            => (numberOfPage - 1) * pageSize;
+    }
+}
