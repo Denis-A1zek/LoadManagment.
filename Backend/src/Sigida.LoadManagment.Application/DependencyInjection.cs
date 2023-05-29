7704217f02ba97d1bc43911fca7a861aae95b228
Denis-A1zek <denissigida.work@gmail.com>
2023-05-29 13:12:33 +0300

    add di to application layer

---
+++ Backend/src/Sigida.LoadManagment.Application/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sigida.LoadManagment.Application.Mappings;
+using Sigida.LoadManagment.Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddMediatR(
+            cfg => cfg.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly));
+
+        services.AddAutoMapper(
+            cfg => cfg.AddProfile(new AssemblyMappingProfile(typeof(IMapWith<>).Assembly)));
+
+        return services;
+    }
+}
