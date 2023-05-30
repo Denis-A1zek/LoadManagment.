af7fa7da630db5a70454e191177960da8333c82d
Denis-A1zek <denissigida.work@gmail.com>
2023-05-30 15:53:30 +0300

    add empoyee page

---
+++ Client/Sigida.LoadManagment.Blazor/Pages/Employee/Employee.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Sigida.LoadManagment.Services.Interfaces;
+using Sigida.LoadManagment.Shared.Models;
+
+namespace Sigida.LoadManagment.Blazor.Pages.Employee
+{
+    public partial class Employee
+    {
+        
+
+        
+    }
+}
