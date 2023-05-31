3d56b6f68a8f90a5ba61a78eabe68fdf171dd7e6
Denis-A1zek <denissigida.work@gmail.com>
2023-05-31 13:30:47 +0300

    add identity server

---
+++ Backend/src/Sigida.LoadManagment.Identity/Models/Configuratuin.cs
@@ -0,0 +1,55 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+
+namespace Sigida.LoadManagment.Identity.Models;
+
+public class Configuratuin
+{
+    public static IEnumerable<ApiScope> ApiScopes
+        => new List<ApiScope>
+        {
+            new ApiScope("LoadManagment", "Web API")
+        };
+
+    public static IEnumerable<IdentityResource> IdentityResources
+        => new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiResource> ApiResources
+        => new List<ApiResource>
+        {
+            new ApiResource("LoadManagment", "WebAPI", new[]{ JwtClaimTypes.Name })
+            {
+                Scopes = {"LoadManagment"}
+            }
+        };
+
+    public static IEnumerable<Client> Clients
+        => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "load-managment-web-api",
+                ClientName = "Load Managment",
+                AllowedGrantTypes = GrantTypes.Code,
+                RequireClientSecret = false,
+                RequirePkce = true,
+                RedirectUris =
+                {
+                    "http://.../signin-iodc"
+                },
+                AllowedCorsOrigins =
+                {
+                    "http://..."
+                },
+                PostLogoutRedirectUris =
+                {
+                    "http://.../signout-oidc"
+                }
+
+            }
+        };
+}
---
+++ Backend/src/Sigida.LoadManagment.Identity/Program.cs
@@ -0,0 +1,33 @@
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddInMemoryClients(new List<Client>())
+    .AddDeveloperSigningCredential();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseIdentityServer();
+
+app.Run();
+
