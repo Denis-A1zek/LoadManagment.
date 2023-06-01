2aca6bd94d0cfdd89034a190d070ee7b65e058bb
Denis-A1zek <denissigida.work@gmail.com>
2023-06-01 20:11:08 +0300

    Create base handler to introduce dependencies

---
+++ Backend/src/Sigida.LoadManagment.Application/Common/Exceptions/BaseHandlerDependencyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Application.Common.Exceptions;
+
+public sealed class BaseHandlerDependencyException : Exception
+{
+    public BaseHandlerDependencyException(string? message, Type type) : base(message)
+    {
+        Message = message;
+        Type = type;
+    }
+    public string Message { get; }
+    public Type Type { get; }
+
+}
---
+++ Backend/src/Sigida.LoadManagment.Application/Features/Base/BaseHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Sigida.LoadManagment.Application.Common.Exceptions;
+using Sigida.LoadManagment.Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Application.Features.Base;
+
+public abstract class BaseHandler
+{
+    private readonly IMapper _mapper;    
+    private readonly ApplicationDbContext _context;
+
+    public BaseHandler(ApplicationDbContext context)
+        => Context = context;
+
+    public BaseHandler(ApplicationDbContext context,IMapper mapper) : this(context)
+        => (_mapper) = (mapper);
+        
+    protected ApplicationDbContext Context
+    {
+        get { return _context ?? throw new BaseHandlerDependencyException(nameof(Context), Context.GetType()); }
+        init { _context = value; }
+    }
+
+    protected IMapper Mapper
+    {
+        get { return _mapper ?? throw new BaseHandlerDependencyException(nameof(Mapper), Mapper.GetType()); }
+        init { _mapper = value; }
+    }
+}
