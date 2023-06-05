dba010e144a600bd0adde11af69cd3468987606b
Denis-A1zek <denissigida.work@gmail.com>
2023-06-05 13:15:17 +0300

    add plan load status

---
+++ Backend/src/Sigida.LoadManagment.Domain/Enum/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Domain.Enum;
+
+public enum Status
+{
+    None = 0,
+    Lecture = 1,
+    Practice = 2,
+    Done = 3
+}
