d0066f9bbae335be3740e829fb91a57c7a5b6fed
Denis-A1zek <denissigida.work@gmail.com>
2023-06-05 13:14:44 +0300

    edit employee

--- Backend/src/Sigida.LoadManagment.Application/Features/EmployeeFeature/CreateEmployee/CreateEmployeeCommand.cs
+++ Backend/src/Sigida.LoadManagment.Application/Features/EmployeeFeature/CreateEmployee/CreateEmployeeCommand.cs
@@ -1,4 +1,9 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using Sigida.LoadManagment.Application.Common.Models;
+using Sigida.LoadManagment.Application.Features.Base;
+using Sigida.LoadManagment.Domain.Entities;
+using Sigida.LoadManagment.Infrastructure.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,3 +15,25 @@ namespace Sigida.LoadManagment.Application.Features;
 public sealed record CreateEmployeeCommand
     (string Name, string Surname, string Lastname, Guid PositionId)
     : IRequest<IResult<Guid>>;
+
+public sealed class CreateEmployeeCommandHandler
+    :BaseHandler ,IRequestHandler<CreateEmployeeCommand, IResult<Guid>>
+{
+    public CreateEmployeeCommandHandler(ApplicationDbContext context) : base(context) { }
+
+    public async Task<IResult<Guid>> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var employee = new Employee()
+        {
+            Name = request.Name,
+            Lastname = request.Lastname,
+            Surname = request.Surname,
+            PositionId = request.PositionId
+        };
+
+        await Context.Set<Employee>().AddAsync(employee);
+        await Context.SaveChangesAsync();
+
+        return Result<Guid>.Success(employee.Id);
+    }
+}
---
+++ Backend/src/Sigida.LoadManagment.Application/Features/EmployeeFeature/CreateEmployee/CreateEmployeeCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigida.LoadManagment.Application.Features.EmployeeFeature.CreateEmployee
+{
+    public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
+    {
+        public CreateEmployeeCommandValidator()
+        {
+            RuleFor(e => e.Name).NotEmpty().Matches("^[А-ЯЁ][А-ЯЁа-яё]*$").MinimumLength(2).MaximumLength(30);
+            RuleFor(e => e.Surname).NotEmpty().Matches("^[А-ЯЁ][А-ЯЁа-яё]*$").MinimumLength(2).MaximumLength(60);
+            RuleFor(e => e.Lastname).NotEmpty().Matches("^[А-ЯЁ][А-ЯЁа-яё]*$").MinimumLength(3).MaximumLength(60);
+            RuleFor(e => e.PositionId).NotEmpty();
+        }
+
+    }
+}
